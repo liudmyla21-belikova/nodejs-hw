7e401403e74c503d50506c57cf865f6d3a0f0b7c
liudmyla21-belikova <liudmyla.belikova@gmail.com>
2025-10-21 11:47:59 +0300

    Fix note.js

--- src/models/note.js
+++ src/models/note.js
@@ -7,10 +7,11 @@ const noteSchema = new Schema(
     title: {
       type: String,
       required: true,
+      trim: true,
     },
     content: {
       type: String,
-      required: false,
+      trim: true,
       default: '',
     },
     tag: {
@@ -24,5 +25,7 @@ const noteSchema = new Schema(
     versionKey: false,
   },
 );
+
+noteSchema.index({ title: 'text', content: 'text' });
 
 export const Note = model('Note', noteSchema);
